9733ae568e12d9ab328fccd707320e86b164e909
Jefferson Rivera <jeff@coink.com>
2020-08-23 19:31:47 -0500

    first commit

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -9,4 +9,12 @@ describe('HelloWorld.vue', () => {
     })
     expect(wrapper.text()).toMatch(msg)
   })
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).
+  })
 })
+
